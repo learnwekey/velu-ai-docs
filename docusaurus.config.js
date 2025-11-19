e8a18dad8dc8a455628e7d350a539a446f4c1e39
vignesh s krishnan <vigneshskrishnan@vigneshs-MacBook-Air.local>
2025-11-19 19:12:05 +0530

    prepare for vercel deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,12 +46,13 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          routeBasePath: '/',                         // docs live at /
+          sidebarPath: require.resolve('./sidebars.js'),
+                              // 👈 make Overview the homepage
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        
         blog: {
           showReadingTime: true,
           feedOptions: {
